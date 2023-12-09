8148c344c557e76994f96c7b4c9e69c433b6080b
gPass0s <guilhermepassos@outlook.com>
2023-12-08 23:51:50 -0300

    Set lambda trigger execution frequency to infinity

--- infra-as-code/main.tf
+++ infra-as-code/main.tf
@@ -66,29 +66,29 @@ module "lambda-dbt-ecs-task-trigger" {
 
   LAMBDA_ENVIRONMENT_VARIABLES = {
     ECS_CLUSTER_NAME         = module.ecs-cluster-for-dbt.cluster_name
-    ECS_TASK_DEFINITIONS_ARN = module.ecs-resources.task-definition-arn
+    ECS_TASK_DEFINITION_ARN  = module.ecs-resources.task-definition-arn
     ECS_TASK_SUBNET_ID       = module.private_subnet_1a.id
     ECS_SECURITY_GROUP_ID    = module.ecs-resources-security-group.id
     SECRET_MANAGER_NAME      = data.aws_secretsmanager_secret.gpassos-snowflake-dbt.id
-    CONTAINERS_NAME          = module.ecs-resources.task-definition-container-name
+    CONTAINER_NAME          = module.ecs-resources.task-definition-container-name
     ENVIRONMENT              = local.ENV
   }
   CREATE_INVOKER_TRIGGER = true
   LAMBDA_EXECUTION_FREQUENCY = {
     dev = {
-      rate  = "5"
+      rate  = "99999999"
       unity = "minutes"
     }
     qa = {
-      rate  = "5"
+      rate  = "99999999"
       unity = "minutes"
     }
     stg = {
-      rate  = "5"
+      rate  = "99999999"
       unity = "minutes"
     }
     prd = {
-      rate  = "5"
+      rate  = "99999999"
       unity = "minutes"
     }
   }
