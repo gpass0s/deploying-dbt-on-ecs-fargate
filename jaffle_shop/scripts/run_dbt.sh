308a5d26d5128108cafc460e2ebddd45f2b7d604
gPass0s <guilhermepassos@outlook.com>
2023-12-08 23:18:35 -0300

    Update sh file that works as entry point for dbt docker container

--- jaffle_shop/scripts/run_dbt.sh
+++ jaffle_shop/scripts/run_dbt.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 # Invoked in container. Will run in ECS Fargate. Called by Orchestration Tool (i.e. Airflow)
 
-echo "Installing dbt deps"
-dbt deps --profiles-dir . --project-dir .
 echo "Running dbt main flow"
 dbt run --profiles-dir . --project-dir .
-echo "Running macro that updates unified model tables"
-dbt run-operation update_unified_model --profiles-dir . --project-dir .
-echo "dbt process has just finished"
