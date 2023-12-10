fb80fb65ef3256ac0e0dee10e79b22d24e184678
gPass0s <guilhermepassos@outlook.com>
2023-12-10 01:21:36 -0300

    Add comment to dbt_container_init_commands.sh file

--- jaffle_shop/dbt_container_init_commands.sh
+++ jaffle_shop/dbt_container_init_commands.sh
@@ -2,4 +2,5 @@
 # Invoked in container. Will run in ECS Fargate. Called by Orchestration Tool (i.e. Airflow)
 
 echo "Running dbt main flow"
+# The --profiles-dir flag is essential for directing dbt execution to the profiles.yml file within this project.
 dbt run --profiles-dir . --project-dir .
