d52f4b2811f58738b58525a014c46f5ab3f5f186
gPass0s <guilhermepassos@outlook.com>
2023-12-11 17:58:38 -0300

    Fix errors and improve backend file of terraform project

--- infra-as-code/backend.tf
+++ infra-as-code/backend.tf
@@ -1,7 +1,21 @@
 terraform {
-  backend "s3" {
-    key     = "quext-data-analytics/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
+
+  backend "s3" {
+    key     = "gpassos-jaffle-shop/terraform.tfstate"
+    encrypt = true
+    region = "us-east-1"
+  }
+
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
 }
